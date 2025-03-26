8a1a6db8f35075ae9850598e5c75c23f7737d217
tsertic <tsertic5@gmail.com>
2025-03-26 14:26:35 +0100

    fixed styling filter options wrap issue

--- src/components/pageSections/projects/ProjectFilter.tsx
+++ src/components/pageSections/projects/ProjectFilter.tsx
@@ -14,7 +14,7 @@ export const ProjectFilter = () => {
     dispatch(filterProjectsByCategory(option));
   };
   return (
-    <ul className=" mx-auto max-w-[600px] flex gap-[20px] justify-center items-center">
+    <ul className=" mx-auto max-w-[600px] flex gap-[20px] max-md:flex-wrap justify-center items-center">
       {FILTER_OPTIONS.map((option) => {
         return (
           <li key={option}>
@@ -22,7 +22,7 @@ export const ProjectFilter = () => {
               onClick={() => {
                 handleFilterButton(option);
               }}
-              className={`inline-block py-[1px] px-[15px] border-[1px] border-transparent bg-base-200 rounded-md shadow-md text-bodyM capitalize hover:bg-neutral hover:text-neutral-content transition-universal ${
+              className={` inline-block py-[1px] px-[15px] border-[1px] border-transparent bg-base-200 rounded-md shadow-md text-bodyM capitalize hover:bg-neutral hover:text-neutral-content transition-universal ${
                 option === selectedFilter && "bg-base-content !text-base-100"
               }`}
             >
